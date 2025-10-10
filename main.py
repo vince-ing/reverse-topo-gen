ce76eb830c791b2f4573c0baf0bda6b783647be5
john-smith229000 <lanceman229000@gmail.com>
2025-10-10 18:59:34 -0500

    plotted topo_04.dat

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#run all code from main
+# main.py
+from visualization.data_load import load_topography
+from visualization.plotting import plot_topography
+from pathlib import Path
+
+def main():
+    topo_file = Path("data/Topo/topo_04.dat")
+    x, z = load_topography(topo_file)
+    plot_topography(x, z, title=topo_file.name, vertical_exaggeration=0)
+
+if __name__ == "__main__":
+    main()
