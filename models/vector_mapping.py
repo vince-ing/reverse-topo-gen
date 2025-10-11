5bb8ac39ea58d47ff3e19bc863dd99a9ed07b91d
john-smith229000 <lanceman229000@gmail.com>
2025-10-11 10:14:33 -0500

    vector mapping instructions

---
+++ models/vector_mapping.py
@@ -0,0 +1,4 @@
+"""Look at vector plotter as an example of how to access the arrays
+        You should match the topography points (topo_x, topo_z) to the end point of the vector (x2, z2), 
+        and translate that corresponding topo point backwards (the beginning of the vector) try nearest neighbor algorithm.
+        To start use topo_04.dat and v_03.dat, this will take us back 5 million years"""
